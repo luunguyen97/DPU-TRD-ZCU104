7b4e5a812a0b2e7892093495b73721d4ecfba49a
Luu Nguyen <47746985+luunguyen97@users.noreply.github.com>
2020-12-23 11:52:26 +0700

    Add files via upload

--- prj/Vivado/scripts/dpux2_zcu104.tcl
+++ prj/Vivado/scripts/dpux2_zcu104.tcl
@@ -1,121 +1,121 @@
 
-################################################################
-# This is a generated script based on design: dpux2
-#
-# Though there are limitations about the generated script,
-# the main purpose of this utility is to make learning
-# IP Integrator Tcl commands easier.
-################################################################
+# ################################################################
+# # This is a generated script based on design: dpux2
+# #
+# # Though there are limitations about the generated script,
+# # the main purpose of this utility is to make learning
+# # IP Integrator Tcl commands easier.
+# ################################################################
 
-namespace eval _tcl {
-proc get_script_folder {} {
-   set script_path [file normalize [info script]]
-   set script_folder [file dirname $script_path]
-   return $script_folder
-}
-}
-variable script_folder
-set script_folder [_tcl::get_script_folder]
+# namespace eval _tcl {
+# proc get_script_folder {} {
+#    set script_path [file normalize [info script]]
+#    set script_folder [file dirname $script_path]
+#    return $script_folder
+# }
+# }
+# variable script_folder
+# set script_folder [_tcl::get_script_folder]
 
-################################################################
-# Check if script is running in correct Vivado version.
-################################################################
-set scripts_vivado_version 2020.1
-set current_vivado_version [version -short]
+# ################################################################
+# # Check if script is running in correct Vivado version.
+# ################################################################
+# set scripts_vivado_version 2020.1
+# set current_vivado_version [version -short]
 
-if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
-   puts ""
-   catch {common::send_gid_msg -ssname BD::TCL -id 2041 -severity "ERROR" "This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."}
+# if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
+#    puts ""
+#    catch {common::send_gid_msg -ssname BD::TCL -id 2041 -severity "ERROR" "This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."}
 
-   return 1
-}
+#    return 1
+# }
 
-################################################################
-# START
-################################################################
+# ################################################################
+# # START
+# ################################################################
 
-# To test this script, run the following commands from Vivado Tcl console:
-# source dpux2_script.tcl
+# # To test this script, run the following commands from Vivado Tcl console:
+# # source dpux2_script.tcl
 
-# If there is no project opened, this script will create a
-# project, but make sure you do not have an existing project
-# <./myproj/project_1.xpr> in the current working folder.
+# # If there is no project opened, this script will create a
+# # project, but make sure you do not have an existing project
+# # <./myproj/project_1.xpr> in the current working folder.
 
-set list_projs [get_projects -quiet]
-if { $list_projs eq "" } {
-   create_project project_1 myproj -part xczu7ev-ffvc1156-2-e
-   set_property BOARD_PART xilinx.com:zcu104:part0:1.1 [current_project]
-}
+# set list_projs [get_projects -quiet]
+# if { $list_projs eq "" } {
+#    create_project project_1 myproj -part xczu7ev-ffvc1156-2-e
+#    set_property BOARD_PART xilinx.com:zcu104:part0:1.1 [current_project]
+# }
 
 
-# CHANGE DESIGN NAME HERE
-variable design_name
+# # CHANGE DESIGN NAME HERE
+# variable design_name
 set design_name dpux2
 
-# If you do not already have an existing IP Integrator design open,
-# you can create a design using the following command:
-#    create_bd_design $design_name
-
-# Creating design if needed
-set errMsg ""
-set nRet 0
-
-set cur_design [current_bd_design -quiet]
-set list_cells [get_bd_cells -quiet]
-
-if { ${design_name} eq "" } {
-   # USE CASES:
-   #    1) Design_name not set
-
-   set errMsg "Please set the variable <design_name> to a non-empty value."
-   set nRet 1
-
-} elseif { ${cur_design} ne "" && ${list_cells} eq "" } {
-   # USE CASES:
-   #    2): Current design opened AND is empty AND names same.
-   #    3): Current design opened AND is empty AND names diff; design_name NOT in project.
-   #    4): Current design opened AND is empty AND names diff; design_name exists in project.
-
-   if { $cur_design ne $design_name } {
-      common::send_gid_msg -ssname BD::TCL -id 2001 -severity "INFO" "Changing value of <design_name> from <$design_name> to <$cur_design> since current design is empty."
-      set design_name [get_property NAME $cur_design]
-   }
-   common::send_gid_msg -ssname BD::TCL -id 2002 -severity "INFO" "Constructing design in IPI design <$cur_design>..."
-
-} elseif { ${cur_design} ne "" && $list_cells ne "" && $cur_design eq $design_name } {
-   # USE CASES:
-   #    5) Current design opened AND has components AND same names.
-
-   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
-   set nRet 1
-} elseif { [get_files -quiet ${design_name}.bd] ne "" } {
-   # USE CASES: 
-   #    6) Current opened design, has components, but diff names, design_name exists in project.
-   #    7) No opened design, design_name exists in project.
-
-   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
-   set nRet 2
-
-} else {
-   # USE CASES:
-   #    8) No opened design, design_name not in project.
-   #    9) Current opened design, has components, but diff names, design_name not in project.
-
-   common::send_gid_msg -ssname BD::TCL -id 2003 -severity "INFO" "Currently there is no design <$design_name> in project, so creating one..."
-
+# # If you do not already have an existing IP Integrator design open,
+# # you can create a design using the following command:
    create_bd_design $design_name
 
-   common::send_gid_msg -ssname BD::TCL -id 2004 -severity "INFO" "Making design <$design_name> as current_bd_design."
-   current_bd_design $design_name
+# # Creating design if needed
+# set errMsg ""
+# set nRet 0
 
-}
+# set cur_design [current_bd_design -quiet]
+# set list_cells [get_bd_cells -quiet]
 
-common::send_gid_msg -ssname BD::TCL -id 2005 -severity "INFO" "Currently the variable <design_name> is equal to \"$design_name\"."
+# if { ${design_name} eq "" } {
+#    # USE CASES:
+#    #    1) Design_name not set
 
-if { $nRet != 0 } {
-   catch {common::send_gid_msg -ssname BD::TCL -id 2006 -severity "ERROR" $errMsg}
-   return $nRet
-}
+#    set errMsg "Please set the variable <design_name> to a non-empty value."
+#    set nRet 1
+
+# } elseif { ${cur_design} ne "" && ${list_cells} eq "" } {
+#    # USE CASES:
+#    #    2): Current design opened AND is empty AND names same.
+#    #    3): Current design opened AND is empty AND names diff; design_name NOT in project.
+#    #    4): Current design opened AND is empty AND names diff; design_name exists in project.
+
+#    if { $cur_design ne $design_name } {
+#       common::send_gid_msg -ssname BD::TCL -id 2001 -severity "INFO" "Changing value of <design_name> from <$design_name> to <$cur_design> since current design is empty."
+#       set design_name [get_property NAME $cur_design]
+#    }
+#    common::send_gid_msg -ssname BD::TCL -id 2002 -severity "INFO" "Constructing design in IPI design <$cur_design>..."
+
+# } elseif { ${cur_design} ne "" && $list_cells ne "" && $cur_design eq $design_name } {
+#    # USE CASES:
+#    #    5) Current design opened AND has components AND same names.
+
+#    set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
+#    set nRet 1
+# } elseif { [get_files -quiet ${design_name}.bd] ne "" } {
+#    # USE CASES: 
+#    #    6) Current opened design, has components, but diff names, design_name exists in project.
+#    #    7) No opened design, design_name exists in project.
+
+#    set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
+#    set nRet 2
+
+# } else {
+#    # USE CASES:
+#    #    8) No opened design, design_name not in project.
+#    #    9) Current opened design, has components, but diff names, design_name not in project.
+
+#    common::send_gid_msg -ssname BD::TCL -id 2003 -severity "INFO" "Currently there is no design <$design_name> in project, so creating one..."
+
+#    create_bd_design $design_name
+
+#    common::send_gid_msg -ssname BD::TCL -id 2004 -severity "INFO" "Making design <$design_name> as current_bd_design."
+#    current_bd_design $design_name
+
+# }
+
+# common::send_gid_msg -ssname BD::TCL -id 2005 -severity "INFO" "Currently the variable <design_name> is equal to \"$design_name\"."
+
+# if { $nRet != 0 } {
+#    catch {common::send_gid_msg -ssname BD::TCL -id 2006 -severity "ERROR" $errMsg}
+#    return $nRet
+# }
 
 set bCheckIPsPassed 1
 ##################################################################
